bacdb14c2b98975195eadc37270e55a6a3fb1d44
Sam Pilehvarian <samp.codes@gmail.com>
2020-12-14 17:22:41 -0500

    Run the initial function of player

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const boxes = Array.from(document.getElementsByClassName("box"))
+const playText = document.getElementById("playText")
+const spaces = [null, null, null, null, null, null, null, null, null]
+const O_TEXT = "0"
+const X_TEXT = "X"
+let currentPlayer = O_TEXT
 console.log(boxes)
 
 const drawBoard = () => {
@@ -17,7 +22,39 @@ const drawBoard = () => {
       styleString += `border-top: 3px solid var(--grey);`
     }
     box.style = styleString
+    box.addEventListener("click", boxClicked)
   })
+}
+
+const boxClicked = (e) => {
+  const id = e.target.id
+  if (!spaces[id]) {
+    spaces[id] = currentPlayer
+    e.target.innerText = currentPlayer
+
+    if (playerHasWon()) {
+      playText.innerText = `${currentPlayer} has won!`
+      return
+    }
+    currentPlayer = currentPlayer === O_TEXT ? X_TEXT : O_TEXT
+  }
+}
+
+const playerHasWon = () => {
+  if (spaces[0] === currentPlayer) {
+    if (spaces[1] === currentPlayer && spaces[2] === currentPlayer) {
+      console.log(`${currentPlayer} wins up top.`)
+      return true
+    }
+    if (spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
+      console.log(`${currentPlayer} wins on the left.`)
+      return true
+    }
+    if (spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
+      console.log(`${currentPlayer} wins diagonally.`)
+      return true
+    }
+  }
 }
 
 drawBoard()
